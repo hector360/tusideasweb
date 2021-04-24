269afe42d9aa53185bee2edf309a995fe9e5cfb2
hectorprogramacion4 <programacion4@sitiocancun.org>
2021-04-23 22:11:06 -0500

    subiendo

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.static(__dirname+'/public'));
 module.exports.io = socketIO(server);
 require('./sockets/socket');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 // app.get('/', (req, res) => {
 //   res.render('principal')
 // });
